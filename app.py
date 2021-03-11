384dc1cd4dae99ba9a2bdb8a1cdd2abe9ff582dd
nathan78906 <nathan.seebarran@gmail.com>
2021-03-10 17:18:13 -0800

    Add high transaction traffic indicator
    - ignore looking for insider trading on high transaction
      traffic stocks; causes too much spam

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ mydb = pymysql.connect(host=os.environ['MARIADB_HOSTNAME'],
 cursor = mydb.cursor()
 
 cursor.execute("call active_watchlist()")
-watchlist = [item[0] for item in cursor]
+watchlist = [{"symbol": item[0], "isHighTransactionTraffic": int(item[1])} for item in cursor]
 cursor.execute("call seen_transactions()")
 transaction_list = [item[0] for item in cursor]
 cursor.execute("call seen_news_releases()")
@@ -27,10 +27,12 @@ news_releases_list = [item[0] for item in cursor]
 
 embeds = []
 
-for symbol in watchlist:
-    new_transactions = process_transactions(mydb, cursor, logger, symbol, transaction_list)
-    embeds += new_transactions
-    new_news_releases = process_news_releases(mydb, cursor, logger, symbol, news_releases_list)
+for item in watchlist:
+    is_high_transaction_traffic = item["isHighTransactionTraffic"]
+    if not is_high_transaction_traffic:
+        new_transactions = process_transactions(mydb, cursor, logger, item["symbol"], transaction_list)
+        embeds += new_transactions
+    new_news_releases = process_news_releases(mydb, cursor, logger, item["symbol"], news_releases_list)
     embeds += new_news_releases
 
 cursor.close()
