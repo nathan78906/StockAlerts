8fd4431552c4d483fd3b22fe0777d046bb42ebfa
nathan78906 <nathan.seebarran@gmail.com>
2021-12-16 16:16:24 -0500

    Add status code 429 to retry request code list

--- requests_retry.py
+++ requests_retry.py
@@ -2,7 +2,7 @@ import requests
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 
-def requests_retry_session(retries=3, backoff_factor=0.3, status_forcelist=range(500, 600), session=None):
+def requests_retry_session(retries=3, backoff_factor=0.3, status_forcelist=(429, 500, 502, 504), session=None):
     session = session or requests.Session()
     retry = Retry(
         total=retries,
